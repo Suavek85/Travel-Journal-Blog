d7bfc2947ed116c13989faa53db3d2e79f5cfe9c
Slawomir Niemiec <slawekniemiec@gmail.com>
2020-12-07 10:51:25 +0100

    Updates

--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-import Provider from './src/helpers/provider';
-
-export const wrapRootElement = Provider;
--- src/components/PostHeader/PostHeader.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import styles from './PostHeader.module.scss'
-
-export default function PostHeader() {
-    return (
-        <h1 className={ styles.header } >Recent travels...</h1>
-    )
-}
